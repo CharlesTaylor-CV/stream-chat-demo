c6277c5173f32d344131d76f94c22f6699423175
Charles Taylor <charles.taylor@collegevine.com>
2022-08-08 21:32:41 -0400

    Work

--- src/backend/populate-db.js
+++ src/backend/populate-db.js
@@ -1,7 +1,7 @@
 const { StreamChat } = require('stream-chat')
 console.log(process.env.REACT_APP_STREAM_KEY, process.env.REACT_APP_STREAM_SECRET)
 const client = StreamChat.getInstance(process.env.REACT_APP_STREAM_KEY, process.env.REACT_APP_STREAM_SECRET)
-const token = serverClient.createToken(process.env.REACT_APP_USER_ID);
+const token = client.createToken(process.env.REACT_APP_USER_ID);
 
 client.upsertUsers([
   { 'id': 'alfred' },
