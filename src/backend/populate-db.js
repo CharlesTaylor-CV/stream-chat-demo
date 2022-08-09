c263f763486b06a5f71474f620a3bad2d68a5480
Charles Taylor <charles.taylor@collegevine.com>
2022-08-09 17:43:09 -0400

    Populate with users & channels

--- src/backend/populate-db.js
+++ src/backend/populate-db.js
@@ -5,6 +5,7 @@ main()
 
 async function main() {
   const client = StreamChat.getInstance(process.env.REACT_APP_STREAM_KEY, process.env.REACT_APP_STREAM_SECRET)
+  // await createChannelTypes(client)
 
   const users = [
     { id: '1', name: 'Alfred', type: 'sem' },
@@ -13,23 +14,14 @@ async function main() {
     { id: '4', name: 'Deborah', type: 'student'},
   ]
 
-  await client.upsertUsers([users])
+  await client.upsertUsers(users)
 
   // Sem to student channels
-
-  // defaults to the same permission set as the built-in 'messaging' permission set
-  // with any modification provided inline.
-  // https://getstream.io/chat/docs/node/channel_features/?language=javascript&q=conversations#channel-types-fields
-  await client.createChannelType({
-    name: 'sem-user-to-student',
-    reactions: false,
-  });
-
-  // can set our own derivable channel id, but not recommended:
-  // https://getstream.io/chat/docs/node/creating_channels/?language=javascript&q=conversations#2.-creating-a-channel-for-a-list-of-members
-
+  upsertChannelType(client, 'sem-user-to-student', {})
   for (let sem of users.filter(u => u.type === 'sem')) {
     for (let student of users.filter(u => u.type == 'student')) {
+      // can set our own derivable channel id, but not recommended:
+      // https://getstream.io/chat/docs/node/creating_channels/?language=javascript&q=conversations#2.-creating-a-channel-for-a-list-of-members
       await client.channel(
         'sem-user-to-student',
         {
@@ -37,6 +29,7 @@ async function main() {
           // Without this array, the channel defaults to a "public" style wherein anyone
           // in our system is allowed to leave a message.
           members: [sem.id, student.id],
+          created_by_id: sem.id,
           // custom metadata goes here:
           // foo: 'bar'
         }
@@ -45,17 +38,25 @@ async function main() {
   }
 
   // SP to student channels
-  await client.createChannelType({
-    name: 'service-provider-to-student',
-  });
+  upsertChannelType(client, 'service-provider-to-student', {})
   for (let sp of users.filter(u => u.type === 'service-provider')) {
     for (let student of users.filter(u => u.type == 'student')) {
       await client.channel(
         'service-provider-to-student',
         {
-          members: [sp.id, student.id]
+          members: [sp.id, student.id],
+          created_by_id: sp.id,
         }
       ).create()
     }
+  }
+}
+
+async function upsertChannelType(client, name, props) {
+  try {
+    await client.createChannelType({ name, ...props})
+  }
+  catch {
+    await client.updateChannelType(name, props)
   }
 }
