2c369fc222c456f79474a44f2021e95ce9fe8aa1
Charles Taylor <charles.taylor@collegevine.com>
2022-08-08 21:34:42 -0400

    .env

--- src/backend/populate-db.js
+++ src/backend/populate-db.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const { StreamChat } = require('stream-chat')
+
 console.log(process.env.REACT_APP_STREAM_KEY, process.env.REACT_APP_STREAM_SECRET)
 const client = StreamChat.getInstance(process.env.REACT_APP_STREAM_KEY, process.env.REACT_APP_STREAM_SECRET)
 const token = client.createToken(process.env.REACT_APP_USER_ID);
