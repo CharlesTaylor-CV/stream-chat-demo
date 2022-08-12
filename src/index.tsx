d813a16575c858d76e3bccb4a48bc84ff3a5fb89
Charles Taylor <charles.taylor@collegevine.com>
2022-08-12 16:31:43 -0400

    Downgrade to react 17 and use npm

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 import App from './App';
 import { getImage } from './assets';
@@ -28,11 +28,11 @@ function ChatApp() {
 }
 
 ReactDOM
-  .createRoot(document.getElementById('root')!)
   .render(
     <React.StrictMode>
       <ChatApp />
     </React.StrictMode>,
+    document.getElementById('root')
   )
 
 // If you want to start measuring performance in your app, pass a function
